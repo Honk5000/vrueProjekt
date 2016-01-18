775771bd5b3b204a0a194883e77728fd083d2f48
Honk5000 <e0726255@student.tuwien.ac.at>
2016-01-18 10:38:14 +0100

    versuch einer eigenen Camera Position für Kinect User

---
+++ Assets/Scripts/VorhangController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VorhangController : MonoBehaviour {
+
+	private Vector3 startPos;
+	private Vector3 translation = new Vector3 (0, 0, 2.5f);
+
+	// Use this for initialization
+	void Start () {
+		//transform.position = transform.position - new Vector3 (0, 0, 5);
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(startPos.z - translation.z < transform.position.z)
+		{
+			transform.position -= translation * Time.deltaTime;
+		}
+	}
+}
